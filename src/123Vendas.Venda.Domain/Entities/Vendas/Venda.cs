894f955ea9bcd8936dad3a5526d82c504d3affb3
hudson-nttdata <hudson.limanascimento@emeal.nttdata.com>
2024-10-13 17:50:05 -0300

    feature: implementado dominio venda

---
+++ src/123Vendas.Venda.Domain/Entities/Vendas/Venda.cs
@@ -0,0 +1,26 @@
+namespace Venda.Domain.Entities
+{
+    public class Venda
+    {
+        public Guid Id { get; set; }
+        public string Numero { get; set; }
+        public DateTime DataVenda { get; set; }
+        // referencia entidade Cliente, usando padrao 'External Identities'
+        public Guid ClienteId { get; set; }
+        public decimal ValorTotal { get; set; }
+        // referencia entidade Filial, usando padrao 'External Identities'
+        public Guid FilialId { get; set; }
+        public List<ItemVenda> Itens { get; set; }
+        public bool Cancelado { get; set; }
+    }
+
+    public class ItemVenda
+    {
+        // referencia entidade Produto, usando padrao 'External Identities'
+        public Guid ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal Desconto { get; set; }
+        public decimal ValorVenda => (Quantidade * ValorUnitario) - Desconto;
+    }
+}
