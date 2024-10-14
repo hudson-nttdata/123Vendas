79d73e98f1eeec4f97be3483eab0942e38b7a568
hudson-nttdata <hudson.limanascimento@emeal.nttdata.com>
2024-10-14 09:32:43 -0300

    feat: adicionado repositorio base para operacoes crud

---
+++ src/123Vendas.Venda.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using _123Vendas.Venda.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace _123Vendas.Venda.Infrastructure.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly VendaContext context;
+
+        public BaseRepository(VendaContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            if (entity != null)
+            {
+                await context.Set<TEntity>().AddAsync(entity);
+            }
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int? id)
+        {
+            var entity = await context.Set<TEntity>().FindAsync(id);
+            return entity!;
+        }
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+
+        public void UpdateAsync(TEntity entity)
+        {
+            context.Set<TEntity>().Update(entity);
+        }
+    }
+}
---
+++ src/123Vendas.Venda.Infrastructure/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace _123Vendas.Venda.Infrastructure.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int? id);
+        void UpdateAsync(TEntity entity);
+        Task SaveChangesAsync();
+        Task DeleteAsync(TEntity entity);
+    }
+}
